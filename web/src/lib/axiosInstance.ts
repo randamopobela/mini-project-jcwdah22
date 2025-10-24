29c00148e81cc3e52310cde15cffcaa1ecf1b48f
khalifallah <ehafi737@gmail.com>
2025-10-24 21:34:34 +0700

    fix(fe/auth): fix auth route

--- web/src/lib/axiosInstance.ts
+++ web/src/lib/axiosInstance.ts
@@ -1,14 +1,13 @@
+import { BASE_API_URL } from "@/config/app.config";
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:
-        `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:8000", // ganti sesuai backend kamu
-    // withCredentials: true, // kalau pakai cookie/session
-    headers: {
-        common: {
-            "Content-Type": "application/json",
-        },
+  baseURL: BASE_API_URL,
+  headers: {
+    common: {
+      "Content-Type": "application/json",
     },
+  },
 });
 
 export default API;
